3de7d5deeb4ad2400b43fb674abb9caf69e1342e
Muchovsky <muchovsky>
2026-02-11 19:56:51 +0100

    2.1 - 2.7 fin

--- TripleX/TripleX/TripleX.cpp
+++ TripleX/TripleX/TripleX.cpp
@@ -1,8 +1,20 @@
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!" << '\n';
+    std::cout << "You are a secret agent braking into a secure server room" << '\n';
+    std::cout << "You need to enter the correct codes to continue ... " << '\n';
+
+    const int a = 123;
+    const int b = 23;
+    const int c = 3;
+
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << '\n';
+    std::cout << product << '\n';
+
     return 0;
 }
