3f006ed29ff9c2e0801e7cdabe27d6b6e3e2a422
Muchovsky <muchovsky>
2026-02-16 14:09:16 +0100

    clenups

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
-//#include "HiddenWordList.h"
-// #include "Math/UnrealMathUtility.h"
+
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -6,11 +6,7 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-//struct FBullCowCount
-//{
-//	int32 Bulls = 0;
-//	int32 Cows = 0;
-//};
+
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
@@ -21,18 +17,9 @@ public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	void ShowWelcomeMessage();
-	//void SetupGame();
-	//void EndGame();
-	//void ProcessGuess(const FString& Guess);
-	//bool IsIsogram(const FString& Word) const;
-	//TArray<FString> GetValidWords(const TArray<FString>& WordList) const;
-	//FBullCowCount GetBullCows(const FString& Guess) const;
 
 	// Your declarations go below!
 private:
 	FString HiddenWord;
-	//FString HiddenWord;
-	//int32 Lives;
-	//bool bGameOver;
-	//TArray<FString> Isograms;
+
 };
